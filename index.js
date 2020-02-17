fd276b9a651a0e19f82d7953c4e0479d882a05d6
lazaronazareno <aragon.ezequiel98@gmail.com>
2020-02-17 13:57:17 -0300

    nuevo archivo js y kata

---
+++ index.js
@@ -0,0 +1,15 @@
+let array1 = [1,2,3];
+let array2 = [2,3,4];
+
+function mergearrays(...arrays){
+    arrays=[];
+
+    
+
+    let newArray=array1.concat(array2);
+
+    
+    console.log(newArray);
+    
+
+}
